50590fb60f9eaacacf2067e78fd4e82316c032a2
Sander <sander12101@gmail.com>
2019-12-11 08:57:50 +0100

    update day3

--- src/day3.rs
+++ src/day3.rs
@@ -3,33 +3,49 @@ use std::num::ParseIntError;
 use std::str::FromStr;
 
 pub fn day3(input: std::string::String) {
-    let mut paths: Vec<Vec<Vector2<i32>>>;
+    let mut paths: Vec<Vec<InputInstruction>> = vec![];
+
+    // hashset and use intersection
 
     for line in input.lines() {
         let line = line
             .trim()
             .split(",")
-            .map(|x| x.parse::<InputInstruction>().unwrap_or_default())
+            .map(|x| x.parse::<InputInstruction>().unwrap())
             .collect::<Vec<_>>();
 
-        println!("{:?}", line);
+        paths.push(line);
     }
 
-    println!("Day 3 Result1: {:?}", 0);
+    println!("Day 3 Result1: {:?}", paths);
     println!("Day 3 Result2: {:?}", 0);
 }
 
-#[derive(Default, Debug)]
+#[derive(Debug)]
 struct InputInstruction {
-    direction: char,
+    direction: Vector2<i32>,
     distance: i32,
+}
+
+impl InputInstruction {
+    fn step(&self) -> Vector2<i32> {
+        self.direction * self.distance
+    }
 }
 
 impl FromStr for InputInstruction {
     type Err = ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let dir = s.chars().nth(0).unwrap_or_default();
+        let dir_char = s.chars().nth(0).unwrap_or_default();
+
+        let dir = match dir_char {
+            'U' => Vector2::new(0, 1),
+            'D' => Vector2::new(0, -1),
+            'L' => Vector2::new(-1, 0),
+            'R' => Vector2::new(1, 0),
+            _ => panic!("Unknown direction {:?}", dir_char),
+        };
 
         let num = s.chars().filter(|x| x.is_digit(10)).collect::<String>();
         let dist = num.parse::<i32>().unwrap_or_default();
