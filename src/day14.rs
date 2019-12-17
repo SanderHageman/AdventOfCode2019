e0167162ea06e9030c067b0793420d9671f32d42
Sander <sander12101@gmail.com>
2019-12-17 23:49:00 +0100

    update day14

--- src/day14.rs
+++ src/day14.rs
@@ -1,35 +1,115 @@
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
+use std::num::ParseIntError;
+use std::str::FromStr;
+
 pub fn day(input: String) {
-    let parsed_input = parse(input);
+    let reactions = parse(input);
+    let mut chemicals = HashSet::<Chemical>::new();
 
-    let result_one = parsed_input;
-    let result_two = 0;
-
-    println!("Day 10 Result1: {:?}", result_one);
-    println!("Day 10 Result2: {:?}", result_two);
-}
-
-fn parse(input: String) -> Vec<Moon> {
-    let moon_data_lines = input.lines().collect::<Vec<_>>();
-    let mut result: Vec<Moon> = vec![];
-
-    // <x=-7, y=17, z=-11>
-    let blacklist = vec!['<', 'x', '=', 'y', 'z', '>'];
-    for line in moon_data_lines {
-        let mut extract = line.to_owned();
-        extract.retain(|c| !blacklist.contains(&c));
-
-        //-7, 17, -11
-        let position_vec = extract
-            .split(',')
-            .map(|s| s.trim().parse::<i32>().unwrap())
-            .collect::<Vec<_>>();
-
-        if position_vec.len() != 3 {
-            panic!("Unable to extract correctly for moon {:?}", line);
+    for reaction in &reactions {
+        for inp in &reaction.input {
+            chemicals.insert(inp.to_owned());
         }
 
-        let position = Vec3::new(position_vec[0], position_vec[1], position_vec[2]);
-        result.push(Moon::new(position));
+        chemicals.insert(reaction.output.to_owned());
+    }
+
+    for chem in chemicals {
+        println!("{:?} => {:?}", chem.name, chem.get_required_ore(&reactions));
+    }
+
+    let result_one = 0;
+    let result_two = 0;
+
+    println!("Day 14 Result1: {:?}", result_one);
+    println!("Day 14 Result2: {:?}", result_two);
+}
+
+impl Chemical {
+    fn get_required_ore(&self, recipes: &Vec<Reaction>) -> u64 {
+        let mut result = 0;
+
+        for recipe in recipes {
+            if recipe.output.name != self.name {
+                continue;
+            }
+
+            for chem in &recipe.input {
+                if chem.name == "ORE" {
+                    result += chem.quantity / self.quantity;
+                }
+
+                result += chem.get_required_ore(&recipes) * chem.quantity;
+            }
+        }
+
+        result
+    }
+}
+
+#[derive(Debug, Clone, Eq)]
+struct Chemical {
+    name: String,
+    quantity: u64,
+}
+
+#[derive(Debug)]
+struct Reaction {
+    input: Vec<Chemical>,
+    output: Chemical,
+}
+
+impl FromStr for Chemical {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let input = s.split_whitespace().collect::<Vec<_>>();
+        assert_eq!(input.len(), 2);
+
+        let chem = Chemical {
+            name: input[1].to_owned(),
+            quantity: input[0].parse::<u64>().unwrap(),
+        };
+
+        Ok(chem)
+    }
+}
+
+impl Hash for Chemical {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
+    }
+}
+
+impl PartialEq for Chemical {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+    }
+}
+
+fn parse(input: String) -> Vec<Reaction> {
+    let mut result: Vec<Reaction> = vec![];
+
+    let input_lines = input.lines().map(|x| x.to_owned()).collect::<Vec<_>>();
+
+    for line in input_lines {
+        let out = line.split("=>").map(|s| s.trim()).collect::<Vec<_>>();
+        assert_eq!(out.len(), 2);
+
+        let inputs_strings = out[0].split(',').map(|s| s.trim()).collect::<Vec<_>>();
+
+        let inputs = inputs_strings
+            .iter()
+            .map(|s| s.parse::<Chemical>().unwrap())
+            .collect::<Vec<_>>();
+
+        let output = out[1].parse::<Chemical>().unwrap();
+
+        result.push(Reaction {
+            input: inputs,
+            output: output,
+        });
     }
 
     result
