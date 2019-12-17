d832fc316ff1d1dc80a6909f5ff2fd3a292ac141
Sander <sander12101@gmail.com>
2019-12-17 11:22:29 +0100

    add float cmp and day ten part two

--- src/day10.rs
+++ src/day10.rs
@@ -1,30 +1,22 @@
 use cgmath::*;
+use float_cmp::ApproxEq;
 use std::collections::HashSet;
 
 pub fn day(input: std::string::String) {
-    let input_vec = input.lines().collect::<Vec<_>>();
-    let mut grid: Vec<Vec<bool>> = vec![vec![]];
-    grid.clear();
-
-    for line in input_vec {
-        grid.push(line.chars().map(|c| c == '#').collect::<Vec<_>>());
-    }
-
-    let w = grid[0].len();
-    let h = grid.len();
-
-    let grid_one_d = input
+    let grid = input
         .chars()
         .filter(|c| !c.is_whitespace())
         .map(|c| c == '#')
         .collect::<Vec<_>>();
 
-    assert_eq!(grid_one_d.len(), w * h);
+    let w = (grid.len() as f64).sqrt() as usize;
+    let h = (grid.len() as f64).sqrt() as usize;
+    assert_eq!(grid.len(), w * h);
 
-    let can_see = grid_one_d
+    let can_see = grid
         .iter()
         .enumerate()
-        .map(|b| get_see(b, w, h, &grid_one_d))
+        .map(|b| get_see(b, w, h, &grid))
         .collect::<Vec<_>>();
 
     let mut max = u32::min_value();
@@ -38,11 +30,34 @@ pub fn day(input: std::string::String) {
         }
     }
 
+    let input_two = max_i;
+    let mut play_grid = grid.clone();
+    let mut destroyed: Vec<usize> = vec![];
+
+    while destroyed.len() <= 200 {
+        let mut angles = get_see_angles(input_two, w, h, &grid)
+            .into_iter()
+            .collect::<Vec<_>>();
+        angles.sort_unstable();
+
+        for angle in angles {
+            let destroy_index = get_closest_by_angle(max_i, angle, w, h, &play_grid);
+
+            assert!(play_grid[destroy_index]);
+            assert!(!destroyed.contains(&destroy_index));
+
+            destroyed.push(destroy_index);
+            play_grid[destroy_index] = false;
+        }
+    }
+
+    let (rtx, rty) = get_xy(destroyed[199], w, h);
+
     let result_one = max;
-    let result_two = 0;
+    let result_two = rtx * 100 + rty;
 
     println!("Day 10 Result1: {:?} {:?}", result_one, get_xy(max_i, w, h));
-    println!("Day 10 Result2: {:?}", result_two);
+    println!("Day 10 Result2: {:?} {:?}", result_two, (rtx, rty));
 }
 
 fn get_see(iter: (usize, &bool), grid_width: usize, grid_height: usize, grid: &Vec<bool>) -> u32 {
@@ -63,7 +78,7 @@ fn get_see_angles(
 ) -> HashSet<i32> {
     let (x_index, y_index) = get_xy(index, grid_width, grid_height);
     let origin = Vector2::new(x_index as f32, y_index as f32);
-    let base = Vector2::new(x_index as f32, y_index as f32 - 1f32);
+    let normal = Vector2::new(0.0, -1f32);
 
     let mut result_angle = HashSet::<i32>::new();
 
@@ -77,12 +92,69 @@ fn get_see_angles(
         let (x, y) = get_xy(i, grid_width, grid_height);
         let target = Vector2::new(x as f32, y as f32);
         let diff = target - origin;
-        let angle = diff.angle(base);
+        let mut angle = -diff.angle(normal).0.to_degrees();
+        if angle < 0.0 {
+            angle = 360.0 + angle;
+        }
+        let cangle = canon_angle(angle);
 
-        result_angle.insert(canon_angle(angle.0));
+        assert!(angle.approx_eq(uncanon_angle(cangle), (0.000001, 1)));
+        result_angle.insert(cangle);
     }
 
     result_angle
+}
+
+fn get_closest_by_angle(
+    origin_index: usize,
+    target_angle: i32,
+    grid_width: usize,
+    grid_height: usize,
+    grid: &Vec<bool>,
+) -> usize {
+    let (x_index, y_index) = get_xy(origin_index, grid_width, grid_height);
+    let origin = Vector2::new(x_index as f32, y_index as f32);
+    let normal = Vector2::new(0.0, -1f32);
+
+    let size = grid_width * grid_height;
+
+    let mut index_and_dist: Vec<(usize, f32)> = vec![];
+
+    for i in 0..size {
+        if i == origin_index || !grid[i] {
+            continue;
+        }
+
+        let (x, y) = get_xy(i, grid_width, grid_height);
+        let target = Vector2::new(x as f32, y as f32);
+        let diff = target - origin;
+        let mut angle = -diff.angle(normal).0.to_degrees();
+        if angle < 0.0 {
+            angle = 360.0 + angle;
+        }
+        let cangle = canon_angle(angle);
+        let mangle = uncanon_angle(target_angle);
+
+        if target_angle != cangle {
+            continue;
+        }
+
+        assert!(mangle.approx_eq(angle, (0.000001, 1)));
+
+        index_and_dist.push((i, diff.magnitude2()));
+    }
+
+    let mut closest = f32::max_value();
+    let mut cindex = 0;
+
+    for (index, dist) in index_and_dist {
+        closest = closest.min(dist);
+        if closest == dist {
+            cindex = index;
+        }
+    }
+
+    cindex
 }
 
 fn get_xy(index: usize, width: usize, height: usize) -> (usize, usize) {
@@ -93,4 +165,8 @@ fn get_xy(index: usize, width: usize, height: usize) -> (usize, usize) {
 
 fn canon_angle(angle: f32) -> i32 {
     (angle * 1024.0 * 1024.0).round() as i32
+}
+
+fn uncanon_angle(canon: i32) -> f32 {
+    canon as f32 / 1024.0 / 1024.0
 }
