277794032ab5ea4247f3983291054a09639f4bf7
Sander <sander12101@gmail.com>
2019-12-19 09:01:37 +0100

    finish day 15 part one

--- src/day15.rs
+++ src/day15.rs
@@ -36,13 +36,6 @@ fn get_part_one(input_vec: &Vec<i64>) -> usize {
     robot.maze.set_tile(Vec2::new(0, 0), Tile::Bot);
     robot.maze.set_tile(robot.maze.robot_pos, Tile::Bot);
     robot.maze.set_tile(robot.maze.oxygen_pos, Tile::Oxy);
-    // robot.maze.draw();
-
-    // let mut result = 0;
-    // while result != 214 {
-    //     result = robot.maze.solve();
-    //     println!("{}", result);
-    // }
 
     robot.maze.solve()
 }
@@ -125,7 +118,6 @@ impl Maze {
             map: HashMap::new(),
             robot_pos: Vec2::new(0, 0),
             oxygen_pos: Vec2::new(0, 0),
-            normalized_cells: Vec::new(),
             solve_cells: Vec::new(),
         }
     }
@@ -202,64 +194,52 @@ impl Maze {
         (x, y)
     }
 
-    ///MAZE SOLVING: Get the next candidate cell
-    fn solution_next(&mut self, cell: &Vec2, w: i64, h: i64) -> Option<Vec2> {
-        self.solve_cells[cell.x as usize][cell.y as usize] = false;
-        let mut neighbors = Vec::new();
+    fn get_next_direction(&mut self, pos: Vec2, offset: Vec2) -> Option<Vec2> {
+        self.solve_cells[pos.y as usize][pos.x as usize] = false;
+        let mut backup = Vec::<Vec2>::new();
 
-        if cell.x > 0
-            && self.solve_cells[(cell.x - 1) as usize][cell.y as usize]
-            && !self.normalized_cells[cell.x as usize][cell.y as usize]
-        {
-            neighbors.push(Vec2::new(cell.x - 1, cell.y));
+        for i in 1..5 {
+            let dir = Dir::from(i);
+            let new_pos = pos + Vec2::from(dir);
+
+            if !self.solve_cells[new_pos.y as usize][new_pos.x as usize] {
+                continue;
+            }
+
+            match self.get_tile(&(new_pos + offset)) {
+                Tile::Empty => backup.push(new_pos),
+                Tile::Bot => backup.push(new_pos),
+                Tile::Oxy => backup.push(new_pos),
+                _ => continue,
+            }
         }
-        if cell.y > 0
-            && self.solve_cells[cell.x as usize][(cell.y - 1) as usize]
-            && !self.normalized_cells[cell.x as usize][cell.y as usize]
-        {
-            neighbors.push(Vec2::new(cell.x, cell.y - 1));
+
+        if backup.is_empty() {
+            return None;
         }
-        if cell.x < w - 2
-            && self.solve_cells[(cell.x + 1) as usize][cell.y as usize]
-            && !self.normalized_cells[cell.x as usize][(cell.y + 1) as usize]
-        {
-            neighbors.push(Vec2::new(cell.x + 1, cell.y));
-        }
-        if cell.y < h - 2
-            && self.solve_cells[cell.x as usize][(cell.y + 1) as usize]
-            && !self.normalized_cells[(cell.x + 1) as usize][cell.y as usize]
-        {
-            neighbors.push(Vec2::new(cell.x, cell.y + 1));
-        }
-        if neighbors.is_empty() {
-            None
-        } else {
-            let next = neighbors.get(random::<usize>() % neighbors.len()).unwrap();
-            Some(*next)
-        }
+
+        let rand = random::<usize>() % backup.len();
+        Some(backup[rand])
     }
+
     //https://www.rosettacode.org/wiki/Maze_solving#Rust
-
-    ///MAZE SOLVING: solve the maze
-    ///Uses self.cells to store the solution cells (true)
     fn solve(&mut self) -> usize {
-        self.normalize_map();
-
         let (minx, miny, maxx, maxy) = self.get_screen_dimensions();
         let w = maxx - minx;
         let h = maxy - miny;
 
-        let offset = Vec2::new(minx, miny);
+        let offset = Vec2::new(miny, minx);
 
         self.solve_cells = vec![vec![true; h as usize]; w as usize];
 
         let mut solution: Vec<Vec2> = Vec::new();
         let mut next = Vec2::new(0, 0) - offset;
+        let last = Vec2::new(self.oxygen_pos.x, self.oxygen_pos.y) - offset;
+
         solution.push(next);
-        let last = self.oxygen_pos - offset;
 
         'main: loop {
-            while let Some(cell) = self.solution_next(&next, w, h) {
+            while let Some(cell) = self.get_next_direction(next, offset) {
                 solution.push(cell);
                 if cell == last {
                     break 'main;
@@ -270,33 +250,13 @@ impl Maze {
             next = *solution.last().unwrap();
         }
 
-        for cell in solution {
-            self.set_tile(cell, Tile::Oxy);
+        for cell in &solution {
+            self.set_tile(cell + offset, Tile::Oxy);
         }
 
         self.draw();
 
-        0
-    }
-
-    fn normalize_map(&mut self) {
-        let (minx, miny, maxx, maxy) = self.get_screen_dimensions();
-        let w = maxx - minx;
-        let h = maxy - miny;
-        let size = w * h;
-
-        self.normalized_cells = vec![vec![false; h as usize]; w as usize];
-        let offset = Vec2::new(minx, miny);
-
-        for i in 0..size {
-            let pos = Vec2::from(Maze::get_xy(i, w));
-            self.normalized_cells[pos.x as usize][pos.y as usize] =
-                match self.get_tile(&(pos + offset)) {
-                    Tile::Wall => false,
-                    Tile::Unknown => false,
-                    _ => true,
-                }
-        }
+        solution.len() - 1
     }
 }
 
@@ -304,7 +264,6 @@ struct Maze {
     map: HashMap<Vec2, Tile>,
     robot_pos: Vec2,
     oxygen_pos: Vec2,
-    normalized_cells: Vec<Vec<bool>>,
     solve_cells: Vec<Vec<bool>>,
 }
 
